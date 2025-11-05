a20d08195a51f26aaf1228962c51c8c67e14d0d6
rethesh9 <rpradhan@ip-172-31-5-155.ap-southeast-2.compute.internal>
2025-11-05 10:12:27 +0000

    Akshay

--- ec2.tf
+++ ec2.tf
@@ -6,5 +6,14 @@ resource "aws_instance" "example" {
   tags = {
     "Name" = var.vm-name
   }
+  provisioner "remote-exec" {
+    
+  }
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+    private_key = aws_key_pair.example
+  }
 }
 
