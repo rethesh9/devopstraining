72b2b7542850592eb982a3ab2f8f9458bba24b9b
rethesh9 <rpradhan@ip-172-31-5-155.ap-southeast-2.compute.internal>
2025-11-05 07:12:34 +0000

    vars change

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0a25a306450a2cba3"
-  instance_type = "t2.nano"
-  key_name      = "roche-key"
+  ami           = var.rethesh-ami-id
+  #"ami-0a25a306450a2cba3"
+  instance_type = var.vm-size
+  key_name      = var.key_name
   tags = {
-    "Name" = "rethesh-vm"
+    "Name" = var.vm-name
   }
 }
 
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "rethesh-ami-id" {
+  type = string
+  description = "desc"
+}
+
+variable "vm-name"{
+    type = string
+}
+
+variable "vm-size" {
+  type =string
+}
+
+variable "key_name" {
+  type = string
+}
