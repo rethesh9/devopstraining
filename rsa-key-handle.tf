e039dd03ae6e590f8cee0724b3efe7e918b50879
retehsh9 <rpradhan@ip-172-31-5-155.ap-southeast-2.compute.internal>
2025-11-05 11:56:57 +0000

    test3

--- rsa-key-handle.tf
+++ rsa-key-handle.tf
@@ -1,9 +1,9 @@
-resource "tls_private_key" "example"{
-    algorithm = var.private-key-algo
-    rsa_bits = var.key-size
+resource "tls_private_key" "example" {
+  algorithm = var.private-key-algo
+  rsa_bits  = var.key-size
 }
 
 resource "aws_key_pair" "example" {
   public_key = tls_private_key.example.public_key_openssh
-  key_name = "rethesh-public-key"
+  key_name   = "rethesh-public-key"
 }
