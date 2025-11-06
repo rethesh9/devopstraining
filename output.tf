f29369b590b1d6b79005e8fc770d99c1340d87cc
retehsh9 <rpradhan@ip-172-31-5-155.ap-southeast-2.compute.internal>
2025-11-06 04:42:39 +0000

    ci to main

--- output.tf
+++ output.tf
@@ -9,8 +9,3 @@ resource "local_file" "my_output_file" {
   depends_on = [aws_instance.example]
 }
 
-resource "local_file" "private_key_data" {
-  content         = tls_private_key.example.private_key_pem
-  filename        = "${path.module}/rethesh-key.pem"
-  file_permission = 0400
-}
--- rsa-key-handle.tf
+++ rsa-key-handle.tf
@@ -5,5 +5,11 @@ resource "tls_private_key" "example" {
 
 resource "aws_key_pair" "example" {
   public_key = tls_private_key.example.public_key_openssh
-  key_name   = "rethesh-public-key"
+  key_name   = vars.key_name
+}
+
+resource "local_file" "private_key_data" {
+  content         = tls_private_key.example.private_key_pem
+  filename        = "${path.module}/rethesh-key.pem"
+  file_permission = 0400
 }
